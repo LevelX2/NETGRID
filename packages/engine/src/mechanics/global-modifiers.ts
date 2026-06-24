ea649207b07b302c1ff658b3276cd4f06a9e633a
LevelX2 <LevelX2@users.noreply.github.com>
2026-06-24 07:19:03 +0200

    refactor(engine): remove stale global modifier constants

--- packages/engine/src/mechanics/global-modifiers.ts
+++ packages/engine/src/mechanics/global-modifiers.ts
@@ -1,28 +1,2 @@
-export const MAIN_OFFICE_RELOCATION_HANDSIZE_AGENDA_ID =
-  "onr_v1_205_main-office-relocation";
-
-export const ICE_TRANSMUTATION_AGENDA_ID = "onr_v1_204_ice-transmutation";
-
-export const BIOWEAPONS_ENGINEERING_CORE_DAMAGE_AGENDA_ID =
-  "onr_v1_190_bioweapons-engineering";
-
-export const FORTRESS_ARCHITECTS_REZ_COST_ASSET_ID =
-  "onr_v1_324_fortress-architects";
-
-export const HACKER_TRACKER_CENTRAL_RUN_LOCK_ASSET_ID =
-  "onr_v1_325_hacker-tracker-central";
-
-export const I_GOT_A_ROCK_BAD_PUBLICITY_ASSET_ID =
-  "onr_v1_327_i-got-a-rock";
-
 export const NEWSGROUP_TAUNTING_TAG_HANDSIZE_ASSET_ID =
   "onr_v1_332_newsgroup-taunting";
-
-export const CITY_SURVEILLANCE_TAG_DAMAGE_ASSET_ID =
-  "onr_v1_313_city-surveillance";
-
-export const SOUTH_AFRICAN_MINING_CORP_ACTION_ASSET_ID =
-  "onr_v1_343_south-african-mining-corp";
-
-export const JERUSALEM_CITY_GRID_REZ_COST_UPGRADE_ID =
-  "onr_v1_360_jerusalem-city-grid";
